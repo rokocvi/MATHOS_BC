a64f247aae92998857e663012b9d81955ba60324
Roko Cvtikovic <cvitkovi.roko5@gmail.com>
2025-06-06 10:37:09 +0200

    Add post and get option

---
+++ BootcampApp/WebAPI/Controllers/BookController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private static List<Book> books = new List<Book>
+        {
+            new Book { Id = 1, Title = "Na Drini čuprija", Author = "Ivo Andrić" },
+            new Book { Id = 2, Title = "Zločin i kazna", Author = "Dostojevski" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBook(int id)
+        {
+            var book = books.FirstOrDefault(b => b.Id == id);
+            if (book == null)
+                return NotFound();
+            return Ok(book);
+        }
+
+        [HttpPost]
+
+        public ActionResult<Book> AddBook([FromBody] Book newBook)
+        {
+            newBook.Id = books.Count > 0 ? books.Max(b => b.Id) + 1 : 1;
+            books.Add(newBook);
+            return CreatedAtAction(nameof(GetBook), new { id = newBook.Id }, newBook);
+        }
+
+
+    }
+
+    
+
+
+    public class Book
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }
+    }
+}
